c42bfbdaea9e84f890a2fbc8b0a1f5db637e62b4
t1mmb0 <timmnicklpl@gmail.com>
2024-12-13 20:13:33 +0100

    Visualization of pc is working
    #9

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import configparser
+config = configparser.ConfigParser()
+from prepare_data import load_data_from_directory, normalize
+import pc_structure as pc
+
+#Visualization of Anomaly Detection for PC
+config.read("config.ini")
+model_dir = config.get("PC_PARAMETERS", "model_dir")
+
+
+
+#Load dataset and prepare
+data_dir = config.get("PATHS", "train_dir")
+data = load_data_from_directory(data_dir)
+
+data = data.map(lambda x,y: (normalize(x),y))
+image_batch, label_batch = next(iter(data))
+label_batch = np.array(label_batch) 
+#Load the model
+anomaly_scores_per_image = np.load(model_dir)
+
+#Visualization of Anomaly Detection for PC
+for i in range(anomaly_scores_per_image.shape[0]):      
+    pc.visualize_anomalies(image_batch[i], anomaly_scores_per_image[i],label_batch[i])  # Visualisiere die Anomalien auf das erste Bild im Batch
